7c09f8fcd67f6c98835b645a0647d1f8ff4709ca
uzair <89578695+Uzairrrrrr@users.noreply.github.com>
2024-07-08 16:57:04 +0500

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(STATIC_URL, 'static/')
+STATIC_ROOT = os.path.join(STATIC_URL, 'static')
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static')
 ]
 MEDIA_ROOT = os.path.join(STATIC_URL, 'media')
 
